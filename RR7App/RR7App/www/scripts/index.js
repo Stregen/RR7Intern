ad680fb3b536873875019eebcc0765d44ed02dbe
Smwarrior <nonsens_4@hotmail.com>
2017-03-27 14:22:12 +0200

    Mhmhmhm
    Soldater-Marie

--- RR7App/RR7App/www/scripts/index.js
+++ RR7App/RR7App/www/scripts/index.js
@@ -1,5 +1,6 @@
 ﻿var counter = 0;
 var sequenceArray = [];
+var binaryArray = [];
 
 (function () {
     "use strict";
@@ -15,53 +16,57 @@ var sequenceArray = [];
 
 
 function init() {
-    var mac = document.getElementById("MAC").value;
-    bluetoothSerial.connectInsecure(mac, success, failure);
-    function success() {
-        alert('Success');
-    }
-    function failure() {
-        alert('Failure');
+    try {
+        var mac = document.getElementById("MAC").value;
+        bluetoothSerial.connectInsecure(mac, success, failure);
+        function success() {
+            alert('Success');
+        }
+        function failure() {
+            alert('Failure');
+        }
+    } catch (err) {
+        alert('Error in BlueTooth. Make sure you have enabled BlueTooth')
     }
 }
 
     function left45Click() {
         console.log("Pressed 45 left");
         sequenceArray.push("../images/left45smol.png");
-        console.log(sequenceArray[counter]);
         counter++;
+        binaryArray.push("0110");
         textAreaFunc();
     }
 
     function left90Click() {
         console.log("Pressed 90 left");
         sequenceArray.push("../images/left90smol.png");
-        console.log(sequenceArray[counter]);
         counter++;
+        binaryArray.push("0101");
         textAreaFunc();
     }
 
     function right45Click() {
         console.log("Pressed right 45");
         sequenceArray.push("../images/right45smol.png");
-        console.log(sequenceArray[counter]);
         counter++;
+        binaryArray.push("0100");
         textAreaFunc();
     }
 
     function right90Click() {
         console.log("Pressed right 90");
         sequenceArray.push("../images/right90smol.png");
-        console.log(sequenceArray[counter]);
         counter++;
+        binaryArray.push("0011");
         textAreaFunc();
     }
 
     function forwardClick() {
         console.log("Pressed forward");
         sequenceArray.push("../images/forwardssmol.png");
-        console.log(sequenceArray[counter]);
         counter++;
+        binaryArray.push("0001");
         textAreaFunc();
     }
 
@@ -70,16 +75,21 @@ function init() {
         sequenceArray.push("../images/backwardssmol.png");
         console.log(sequenceArray[counter]);
         counter++;
+        binaryArray.push("0010");
         textAreaFunc();
     }
 
     function clearClick() {
-        console.log("Pressed clear");
         for (var i = 0; i < counter; i++) {
             sequenceArray.pop();
         }
+
+        for (var i = 0; i < counter; i++) {
+            binaryArray.pop();
+        }
+
         counter = 0;
-        console.log("Pressed empty. Array and counter resat");
+        console.log("Pressed clear. Array and counter resat");
         document.getElementById("Textbox").innerHTML = "";
     }
 
@@ -88,7 +98,10 @@ function init() {
         var buffer = "Placeholder - this button doesn't do anything... YET!";
         document.getElementById("Textbox").innerHTML = datadump + buffer;
         console.log("Pressed start");
-        bluetoothSerial.write("Hej123");
+        binaryArray.push("0000,");
+        var stringToSend = binaryArray.toString();
+        bluetoothSerial.write(stringToSend);
+        console.log(stringToSend);
     }
 
     function undoClick() {
@@ -96,12 +109,16 @@ function init() {
         counter--;
         console.log("Pressed undo");
         textAreaFunc();
+        binaryArray.pop();
+        if (counter < 0) { counter = 0 };
     }
 
     //Add images to DIV
     function textAreaFunc() {
-        document.getElementById("Textbox").innerHTML = "";
         console.log(counter);
+        console.log(sequenceArray);
+        console.log(binaryArray);
+        document.getElementById("Textbox").innerHTML = "";
         for (i = 0; i < counter; i++) {
             //var s = sequenceArray[counter]
             //var imgPath = "<img src='" + s + "' />";
@@ -112,7 +129,6 @@ function init() {
             //New try
             var img = document.createElement('img');
             img.setAttribute('src', sequenceArray[i]);
-            console.log(img.src);
             document.getElementById("Textbox").appendChild(img);
 
         }
