a519a25a6fb09c8b76323617e037dff998421e28
Stregen <nikolajbilgrau@gmail.com>
2017-03-27 15:49:52 +0200

    I'm done
    I'm fucking done

--- RR7App/RR7App/www/scripts/index.js
+++ RR7App/RR7App/www/scripts/index.js
@@ -33,49 +33,48 @@ function init() {
     function left45Click() {
         console.log("Pressed 45 left");
         sequenceArray.push("../images/left45smol.png");
-        counter++;
         binaryArray.push("0110");
+        counter++;
         textAreaFunc();
     }
 
     function left90Click() {
         console.log("Pressed 90 left");
         sequenceArray.push("../images/left90smol.png");
-        counter++;
         binaryArray.push("0101");
+        counter++;
         textAreaFunc();
     }
 
     function right45Click() {
         console.log("Pressed right 45");
         sequenceArray.push("../images/right45smol.png");
-        counter++;
         binaryArray.push("0100");
+        counter++;
         textAreaFunc();
     }
 
     function right90Click() {
         console.log("Pressed right 90");
         sequenceArray.push("../images/right90smol.png");
-        counter++;
         binaryArray.push("0011");
+        counter++;
         textAreaFunc();
     }
 
     function forwardClick() {
         console.log("Pressed forward");
         sequenceArray.push("../images/forwardssmol.png");
-        counter++;
         binaryArray.push("0001");
+        counter++;
         textAreaFunc();
     }
 
     function backwardsClick() {
         console.log("Pressed backwards");
         sequenceArray.push("../images/backwardssmol.png");
-        console.log(sequenceArray[counter]);
-        counter++;
         binaryArray.push("0010");
+        counter++;
         textAreaFunc();
     }
 
@@ -84,7 +83,7 @@ function init() {
             sequenceArray.pop();
         }
 
-        for (var i = 0; i < counter; i++) {
+        for (var i = 0; i < counter+1; i++) {
             binaryArray.pop();
         }
 
@@ -94,10 +93,10 @@ function init() {
     }
 
     function startClick() {
+        console.log("Pressed start");
         var datadump = document.getElementById("Textbox").value;
         var buffer = "Placeholder - this button doesn't do anything... YET!";
         document.getElementById("Textbox").innerHTML = datadump + buffer;
-        console.log("Pressed start");
         binaryArray.push("0000,");
         var stringToSend = binaryArray.toString();
         bluetoothSerial.write(stringToSend);
@@ -105,18 +104,18 @@ function init() {
     }
 
     function undoClick() {
-        sequenceArray.pop();
-        counter--;
         console.log("Pressed undo");
-        textAreaFunc();
+        sequenceArray.pop();
         binaryArray.pop();
+        counter--;
         if (counter < 0) { counter = 0 };
+        textAreaFunc();
     }
 
     //Add images to DIV
     function textAreaFunc() {
         console.log(counter);
-        console.log(sequenceArray);
+        console.log(sequenceArray[counter-1]);
         console.log(binaryArray);
         document.getElementById("Textbox").innerHTML = "";
         for (i = 0; i < counter; i++) {
